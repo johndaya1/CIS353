591d9bfe2b9ef21777678a3654680f11b5ec5266
Dayaseh (DJ) <71980784+johndaya1@users.noreply.github.com>
2022-02-28 18:39:22 -0500

    Update companyDML.sql

--- companyDML.sql
+++ companyDML.sql
@@ -44,6 +44,7 @@ For every employee who works more than 30 hours on any project: Find the ssn, ln
 SELECT Ssn, Lname,Pnumber, Pname, Hours
 FROM EMPLOYEE, PROJECT, WORKS_ON
 WHERE Hours > 30.0
+GROUP BY Ssn, Lname,Pnumber, Pname, Hours
 ORDER BY Ssn;
 --
 -- JOINING 3 TABLES ---------------------------
