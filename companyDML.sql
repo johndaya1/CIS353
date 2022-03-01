d57ebea93b7d0a2adff54d617bc6df050b37d76b
Dayaseh (DJ) <71980784+johndaya1@users.noreply.github.com>
2022-03-01 16:52:37 -0500

    Update companyDML.sql

--- companyDML.sql
+++ companyDML.sql
@@ -52,7 +52,7 @@ ORDER BY Ssn;
 Write a query that consists of one block only.
 For every employee who works on a project that is not controlled by the department they work for: Find the employee's lname, the department they works for, the project number that they work on, and the number of the department that controls that project. Sort the results by lname.
 */
-SELECT Lname, Dno, Dnum, Pno, Essn, Ssn, Pnumber
+SELECT Lname, Dno, Pno, Pnumber
 FROM EMPLOYEE, PROJECT, WORKS_ON
 WHERE Essn = Ssn AND Pno = Pnumber AND Dno != Dnum
 ORDER BY Lname;
